df1b4983bf3f87fb702c5d51bc207e0054254950
kasaconinmuru <33105775+kasaconinmuru@users.noreply.github.com>
2017-10-26 01:17:35 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,49 @@
+# Importamos las librerías necesarias
+from telegram.ext import Updater
+ 
+# Método que imprimirá por pantalla la información que reciba
+def listener(bot, update):
+    id = update.message.chat_id
+    mensaje = update.message.text
+ 
+    print("ID: " + str(id) + " MENSAJE: " + mensaje)
+ 
+# Método que utilizaremos para cuando se mande el comando de "start"
+def start(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text='¡Bienvenido al bot de Bytelix!')
+ 
+# Método que mandará el mensaje "¡Hola, lector de Bytelix!"
+def hola_mundo(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text='¡Hola, lector de Bytelix!')
+ 
+# Método que mandará el logo de la página
+def logo(bot, update):
+    # Enviamos de vuelta una foto. Primero indicamos el ID del chat a donde
+    # enviarla y después llamamos al método open() indicando la dónde se encuentra
+    # el archivo y la forma en que queremos abrirlo (rb = read binary)
+    bot.sendPhoto(chat_id=update.message.chat_id, photo=open('Icono.png', 'rb'))
+ 
+def main():
+    # Creamos el Updater, objeto que se encargará de mandarnos las peticiones del bot
+    # Por supuesto no os olvidéis de cambiar donde pone "TOKEN" por el token que os ha dado BotFather
+    updater = Updater("TOKEN")
+ 
+    # Cogemos el Dispatcher, en el cual registraremos los comandos del bot y su funcionalidad
+    dispatcher = updater.dispatcher
+ 
+    # Registramos el método que hemos definido antes como listener para que muestre la información de cada mensaje
+    listener_handler = MessageHandler(Filters.text, listener)
+    dispatcher.add_handler(listener_handler)
+ 
+    # Ahora registramos cada método a los comandos necesarios
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("holamundo", hola_mundo))
+    dispatcher.add_handler(CommandHandler("logo", logo))
+ 
+    # Y comenzamos la ejecución del bot a las peticiones
+    updater.start_polling()
+    updater.idle()
+ 
+# Llamamos al método main para ejecutar lo anterior
+if __name__ == '__main__':
+    main()
